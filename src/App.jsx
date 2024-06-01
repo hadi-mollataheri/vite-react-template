d37f61b8ca05d0675cafe1d729ac23d273d78558
Hadi <mhadimollataheri@gmail.com>
2024-06-01 10:58:51 +0330

    Configure primary structure -Remove uncessary code -Change dev server port to 3000 -Install tailwindcss and postcss and initialize them -Create git project or repo

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+function App() {
+  return (
+    <div>
+      <p className=' bg-green-500 font-bold'>
+        Click on the Vite and React logos to learn more
+      </p>
+    </div>
+  );
+}
+
+export default App;
+
